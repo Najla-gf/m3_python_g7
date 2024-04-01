b2e66a7734a4f8cd35c6c95dcebf5a2ecbc1154a
Najla Gatica <najla.gatica@gmail.com>
2024-04-01 20:33:36 -0300

    Pizza app - Codigo Refactorizado

--- Dia14/pizza_app/main.py
+++ Dia14/pizza_app/main.py
@@ -7,102 +7,77 @@ from add import agregar_ingrediente
 from remove import quitar_ingrediente
 from show import mostrar_ingredientes
 from tiempo import estimar_tiempo
-
+import datos as d
+import time
+import os
+import sys
+clear = "cls" if sys.platform == "win32" else "clear"
 
 #Diccionario para diferenciar la masa, salsa y la lista de ingredientes
-ingredientes_orden = {'masa': 'Masa Tradicional', 
-                    'salsa': 'Salsa de Tomate', 
-                    'ingredientes': ['Queso'] 
-                    } 
+ingredientes_orden = d.ingredientes
 
 #Se define el menú infito para que se muestre siempre:
 print('¡Gracias por preferirnos!')
 while True:
-    opcion = opcion = input('''¿Qué desea realizar? 
-                1. Cambiar tipo de Masa 
-                2. Cambiar tipo de Salsa 
-                3. Agregar Ingredientes 
-                4. Eliminar Ingredientes 
-                5. Ordenar con los Ingredientes Actuales 
-                
-                0. Consultar ingredientes de la pizza 
-                
-                Otro Número cancelará el pedido. 
-                > ''') 
+    os.system(clear) #Con esto vamos limpiando la terminar con cada menu
+    opcion = input(d.menu) 
 
 #Mientras tanto se definen todas las funciones en modulos aparte
 #luego de generar todos los scripts, volvemos al archivo principal para invocarlos
 
 #1. Cambiar masa:
     if opcion == '1':
-        eleccion = input("""Escoja el tipo de Masa:
-            T). Masa Tradicional
-            D). Masa Delgada
-            B). Masa con Bordes de Queso
-            > """).upper()
+        os.system(clear)
+        eleccion = input(d.T_MASA).upper()
         ingredientes_orden = tipo_masa(ingredientes_orden, eleccion)
-
+        print("") #agregamos un salto de linea
 
 #2. Cambiar salsa:
     elif opcion == '2':
-        eleccion = input('''Seleccione su tipo de Salsa:
-            T). Salsa de Tomate
-            A). Salsa Alfredo
-            B). Salsa Barbecue
-            P). Salsa Pesto
-            > ''').upper()
+        os.system(clear)
+        eleccion = input(d.T_SALSA).upper()
         ingredientes_orden = tipo_salsa(ingredientes_orden, eleccion)
-
+        print("")
 
 #3. Agregar ingrediente:
     elif opcion == '3':
-        eleccion = int(input('''Seleccione su Ingrediente:
-            1). Tomate
-            2). Champiñones
-            3). Aceituna
-            4). Cebolla
-            5). Pollo
-            6). Jamón
-            7). Carne
-            8). Tocino
-            9). Queso
-            > '''))
+        os.system(clear)
+        eleccion = int(input(d.AG_INGREDIENTE))
         ingredientes_orden = agregar_ingrediente(ingredientes_orden,eleccion)
-
+        print("")
 
 #4. Eliminar ingrediente:
     elif opcion == '4':
-        eleccion = int(input('''Seleccione qué ingrediente quitar:
-            1). Tomate
-            2). Champiñones
-            3). Aceituna
-            4). Cebolla
-            5). Pollo
-            6). Jamón
-            7). Carne
-            8). Tocino
-            9). Queso
-            > '''))
+        os.system(clear)
+        eleccion = int(input(d.QT_INGREDIENTE))
         ingredientes_orden = quitar_ingrediente(ingredientes_orden,
         eleccion)
-
+        print("")
 
 #5. Hacer la orden:
     elif opcion == '5':
+        os.system(clear)
         tiempo = estimar_tiempo(ingredientes_orden)
         print(f'Su Pizza estará lista en {tiempo} minutos')
         ordenar = input('Desea ordenar ahora S/N: ').upper()
-    
+        print("")
+        
         if ordenar == 'S':
             print('Gracias por ordenar en Pizza JAT')
             print('Disfrute su Pizza')
+            time.sleep(3) #Los mensajes de finalización se mantienen 3 seg.
             exit()
 
 #0. Mostrar ingredientes
     elif opcion == '0':
+        os.system(clear)
         mostrar_ingredientes(ingredientes_orden)
+        time.sleep(3)
+        print("")
 
 #Cancelar el pedido
     else:
+        os.system(clear)
         print('Su pedido ha sido cancelado. Gracias por contactarse a Pizza JAT')
+        time.sleep(3)
         exit()
